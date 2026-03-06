fb363c7d9dfa90de74c9f1d28dae31acdc0ce295
MikeProStudio <m.neumueller93@gmx.de>
2026-03-06 23:38:21 +0100

    Remove build.bat from tracking

--- build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set ZEPHYR_BASE=C:\ncs\v3.2.3\zephyr
-set PYTHONPATH=C:\ncs\toolchains\fd21892d0f\opt\bin\Lib\site-packages
-set PATH=C:\ncs\toolchains\fd21892d0f\opt\bin;C:\ncs\toolchains\fd21892d0f\opt\bin\Scripts;%PATH%
-west build -b xiao_nrf54l15/nrf54l15/cpuapp -p always
